2f39fdabd35189e9ccb0cf7021f59f1ba8fdc02b
Tanishfish <tanishbehl36.tb@gmail.com>
2023-09-26 15:28:25 +0530

    first

--- index.js
+++ index.js
@@ -5,19 +5,49 @@ const sliderRouter = require('./Router/slider.route');
 const blogRouter = require('./Router/blog.route');
 const formRouter = require('./Router/form.route');
 const contentRouter = require('./Router/content.route');
+const path = require("path");
 
 if (process.env.NODE_ENV === "development") {
     require("dotenv").config();
-  }
+}
 
 const app = express();
 const PORT = 4321;
 
 app.use(cors());
 
+// Serve static files from the "downloads" directory
+app.use("/downloads", express.static(path.join(__dirname, "downloads")));
+
+app.get("/api/listImages", (req, res) => {
+    const uploadDirectory = path.join(__dirname, "downloads");
+    const imageUrls = [];
+
+    // Use the 'fs' module to read the files in the 'downloads' directory
+    const fs = require("fs");
+
+    fs.readdir(uploadDirectory, (err, files) => {
+        if (err) {
+            console.error("Error reading directory:", err);
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+
+        // Assuming all files in 'downloads' are images
+        files.forEach((file) => {
+            // Construct the URLs for the images
+            const imageUrl = `/downloads/${file}`;
+            imageUrls.push(imageUrl);
+        });
+
+        // Send the URLs as a JSON response
+        res.json({ imageUrls });
+    });
+});
+
+
 app.get('/', (req, res) => {
     res.json({
-        msg: 'Api Created Successfully',
+        msg: 'API Created Successfully',
     });
 });
 
